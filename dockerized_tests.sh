d11d54d364ae7ed19cc3c2149cd128f135c80fd4
Martin Hughes <martin-hughes@users.noreply.github.com>
2024-09-16 20:12:20 +0100

    Test on 8.4
    This introduced a bunch of deprecation warnings. Upgrading to PHPUnit
    9.6 removed all of the warnings relating to dependencies

--- dockerized_tests.sh
+++ dockerized_tests.sh
@@ -7,9 +7,10 @@ set -o pipefail
 TEST_IMAGES=(
   "php:7.4.33-cli-buster"
   "php:8.0.30-cli-buster"
-  "php:8.1.26-cli-bookworm"
-  "php:8.2.13-cli-bookworm"
-  "php:8.3.0-cli-bookworm"
+  "php:8.1.29-cli-bookworm"
+  "php:8.2.23-cli-bookworm"
+  "php:8.3.11-cli-bookworm"
+  "php:8.4.0beta5-cli-bookworm"
 )
 
 FAILED=()
